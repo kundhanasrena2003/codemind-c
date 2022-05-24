949f8aaa77b52cc9cf1d82c2d485a8536f0c1ad5
kundhanasrena2003 <97425765+kundhanasrena2003@users.noreply.github.com>
2022-05-24 15:35:03 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {           scanf("%d",&arr[i]);     }       for(i=1;i<=n;i++)       {           for(j=1;j<=i;j++)           {               printf("%d",j);           }           printf(" ");       }       return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 int main()
 {
- int n,arr[100],i,j;
- scanf("%d",&n);
- for(i=1;i<=n;i++)
- {
-     for(j=1;j<=n;j++)
-     {
-         printf("%c ",'A'-1+i);
-     }
-     printf("
+    int n,arr[100],i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+          scanf("%d",&arr[i]);
+    }
+      for(i=1;i<=n;i++)
+      {
+          for(j=1;j<=i;j++)
+          {
+              printf("%d",j);
+          }
+          printf("
 ");
- }
+      }
+      return 0;
 }

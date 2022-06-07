e56b80d5f9c20d7371c1b6e9d7a52d84c3c150df
kundhanasrena2003 <97425765+kundhanasrena2003@users.noreply.github.com>
2022-06-07 16:20:08 +0530

    #include<stdio.h> int main() {     char str[100];     int n,i,max;     //scanf("%d",&n);     scanf("%[^ ]s",str);     max=str[0];     for(i=0;str[i]!=NULL;i++)     {         if(max<str[i])         {             max=str[i];         }     }     printf("%c",max);     return 0; }

---
+++ max_of_string.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int n,i,max;
+    //scanf("%d",&n);
+    scanf("%[^
+]s",str);
+    max=str[0];
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(max<str[i])
+        {
+            max=str[i];
+        }
+    }
+    printf("%c",max);
+    return 0;
+}

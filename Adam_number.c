ee338670bf7eb62d3d6477c6b053c21a5fb458ad
kundhanasrena2003 <97425765+kundhanasrena2003@users.noreply.github.com>
2022-06-28 22:33:58 +0530

    #include<stdio.h> #include<math.h> int main() { int n,sum=0,r,p,j,i=0,x; scanf("%d",&n); p=n*n; while (n>0) { r=n%10; sum=sum*10+r; n=n/10; } x=sum*sum; while (x>0) { j=x%10; i=i*10+j; x=x/10; } if(p==i) { printf("True"); } else { printf("False"); } return 0; }

--- Adam_number.c
+++ Adam_number.c
@@ -1,31 +1,30 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int n,i,k,d,s=0,temp,p,temp2,z=0;
-    scanf("%d",&n);
-    temp=n;
-    k=n*n;
-    while(n!=0)
-    {
-        d=n%10;
-        n=n/10;
-        s=s*10+d;
-    }
-    p=s*s;
-    temp2=p;
-    while(p!=0)
-    {
-        d=p%10;
-        p=p/10;
-        z=z*10+d;
-    }
-    if(z==k)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
-    return 0;
+int n,sum=0,r,p,j,i=0,x;
+scanf("%d",&n);
+p=n*n;
+while (n>0)
+{
+r=n%10;
+sum=sum*10+r;
+n=n/10;
+}
+x=sum*sum;
+while (x>0)
+{
+j=x%10;
+i=i*10+j;
+x=x/10;
+}
+if(p==i)
+{
+printf("True");
+}
+else
+{
+printf("False");
+}
+return 0;
 }

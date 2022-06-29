a68fd1b3dd353cebb1bf2cf5e68f7d055c55289a
kundhanasrena2003 <97425765+kundhanasrena2003@users.noreply.github.com>
2022-06-29 22:16:09 +0530

    #include<stdio.h> int main() {     int n,i,c=1;     scanf("%d",&n);     int ar[n];     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);         //printf("%d ",ar[i]);     }     //printf("");     for(i=0;i<n;i++)     {         if(i+1<n)         {             if(ar[i]<ar[i+1])             {                // printf("%d %d if",ar[i],ar[i+1]);                 c++;             }             else             {             //printf("%d %d else",ar[i],ar[i+1]);             break;             }         }     }     //printf("%d %d",c,n);     if(c==n)         printf("yes");     else         printf("no"); }

---
+++ Ascending_array.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=1;
+    scanf("%d",&n);
+    int ar[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+        //printf("%d ",ar[i]);
+    }
+    //printf("");
+    for(i=0;i<n;i++)
+    {
+        if(i+1<n)
+        {
+            if(ar[i]<ar[i+1])
+            {
+               // printf("%d %d if",ar[i],ar[i+1]);
+                c++;
+            }
+            else
+            {
+            //printf("%d %d else",ar[i],ar[i+1]);
+            break;
+            }
+        }
+    }
+    //printf("%d %d",c,n);
+    if(c==n)
+        printf("yes");
+    else
+        printf("no");
+}
